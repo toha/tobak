f13cda6d9f05254025a78cbfe3d88d70c1b55c7c
Tobias Hartwich <mail@tha.io>
2016-01-05 23:05:43 +0100

    added building of duplicity command and replacing $VARS in duplicity profile

--- py/modules/duplicity.py
+++ py/modules/duplicity.py
@@ -6,6 +6,36 @@ DUPLICITY_BASE_COMMAND = "duplicity"
 DUPLICITY_BASE_OPTS = " --asynchronous-upload --volsize 100 --num-retries 3 --verbosity 4 --exclude '**/*'"
 
 
+def replaceModuleConfigVars(modulecfg, sched):
+	return modulecfg.replace("$SCHEDNAME", sched["name"])
+
+
+def createDuplicityCommand(modulecfg):
+	if not modulecfg.has_key("include_files") or not modulecfg.has_key("destination"):
+		raise Exception("TODO ERROR")
+
+
+	base_cmd = DUPLICITY_BASE_COMMAND
+
+
+
+	opts = ""
+	if modulecfg.has_key("log_file"):
+		opts += " --log-file %s" %(modulecfg["log_file"])
+	if modulecfg.has_key("encrypt_key"):
+		opts += " --hidden-encrypt-key %s" %(modulecfg["encrypt_key"])
+	if modulecfg.has_key("signing_key"):
+		opts += " --sign-key %s" %(modulecfg["signing_key"])
+	if modulecfg.has_key("full_if_older_than"):
+		opts += " --full-if-older-than %s" %(modulecfg["full_if_older_than"])
+
+	for include_file in modulecfg["include_files"]:
+		opts += " --include '%s'" %include_file
+
+	full_command = "%s%s%s / %s" %(base_cmd, opts, DUPLICITY_BASE_OPTS, modulecfg["destination"])
+	return full_command
+
+
 class Duplicity( Module ):
 
 	def run( self, sched ):
